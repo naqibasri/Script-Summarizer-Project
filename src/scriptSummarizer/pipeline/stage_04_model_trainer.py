858b06835fe7f36d25d88dcb151ee2015a4ba8ee
naqib <naqib.asri.294@gmail.com>
2023-07-04 10:02:52 +0800

    Model trainer completed

---
+++ src/scriptSummarizer/pipeline/stage_04_model_trainer.py
@@ -0,0 +1,13 @@
+from scriptSummarizer.config.configuration import ConfigurationManager
+from scriptSummarizer.conponents.model_trainer import ModelTrainer
+from scriptSummarizer.logging import logger
+
+class ModelTrainerTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        model_trainer_config = config.get_model_trainer_config()
+        model_trainer_config = ModelTrainer(config=model_trainer_config)
+        model_trainer_config.train()
